02c03bbe45cd6a6149b0485e5d03a61e4d20577e
dbaylyuvic <dbayly@uvic.ca>
2018-02-13 14:41:55 -0800

    More advanced template

--- server.js
+++ server.js
@@ -14,16 +14,47 @@ client.userInfo(function (err, data) {
     return err;
   }
 });
-client.blogLikes('deepspacebutthole',{limit:20},function(err,data){
+client.blogLikes('deepspacebutthole',{limit:200},function(err,data){
   if(err){
     console.log(err);
   }else{
     console.log(data.liked_count);
     for(let x in data.liked_posts){
       console.log(data.liked_posts[x].reblog_key);
-      client.reblogPost('dsplikes',{id:data.liked_posts[x].id,state:'queue',reblog_key:data.liked_posts[x].reblog_key},function(res){
-        console.log(res);
-      });
+      /**/
     }
   }
-})
+});
+
+let likes = [];
+function amassList(offset){
+  return new Promise((resolve,reject)=>{
+
+    if(offset>2000){
+      //console.log(likes);
+        resolve(true);
+
+    }
+    //console.log((offset/50) + "Passes");
+    client.blogLikes('deepspacebutthole',{limit:50,offset:offset},function(err,data){
+    console.log((offset/50) + "Passes");
+      if(err){
+        reject(err);
+      }
+      for(let x in data.liked_posts){
+        likes.push(data.liked_posts[x]);
+      }
+      amassList(offset+50).then(function(result){
+        resolve(result);
+      });
+    })
+  });
+
+}
+amassList(50).then(function(result){
+  for(let x in likes){
+      client.reblogPost('dsblikes',{id:likes[x].id,state:'queue',reblog_key:likes[x].reblog_key},function(res){
+          console.log(res);
+      });
+  }
+});
